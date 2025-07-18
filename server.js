ac2b465a96388cddf064792f25044a052f021b8c
Nos <zvezdazanatih@gmail.com>
2025-07-18 13:45:58 +0700

    15

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ wss.on('connection', (ws) => {
 
   const d={a:'blabla', b:'kuku', c:1974, d:true}
   let m="Hello!!!!!!!!!!!!!"
-  const message =JSON.stringify({t1:'myt', d1:d, k1:2028, m:m});
+  const message =JSON.stringify({t1:'myt', d1:d, k1:2028, m:m, e:"Connected"});
 
   clients.forEach((client) => {
     if(client.readyState === WebSocket.OPEN){
